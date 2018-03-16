cb46fd7c031251029fb166193d5a9b98c603a57d
Simon Atkins <this-is-simon@github.com>
2018-03-16 16:39:54 +0000

    written more Room tests

--- room.rb
+++ room.rb
@@ -5,6 +5,12 @@ class Room
     #We're only passing one parameter because we've decided we only want to set a name for a 'newed up' room (Room 101) and not any guests
     @name = input_name
     @occupants = []
+    @playlist = []
+  end
+
+  def add_guest_to_room(guest_object)
+    #We can call the parameter anything we like. It refers back to the parameter in our test_add_guest_to_room(@guest1)
+    return @occupants << guest_object
   end
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,6 +7,15 @@ class RoomTest < MiniTest::Test
 
   def setup
     @room1 = Room.new(101)
+
+    @guest1 = Guest.new("Mike")
+    @guest2 = Guest.new("Simon")
+    @guest3 = Guest.new("Lewis")
+
+    #copied and pasted from our guest_spec.rb
+    @song1 = Song.new("Let Me Put My Love Into You", "ACDC")
+    @song2 = Song.new("Song 2", "Blur")
+    #copied and pasted from our song_spec.rb
   end
 
   def test_room_has_name
@@ -19,8 +28,27 @@ class RoomTest < MiniTest::Test
   end
 
   def test_room_has_playlist
-    result = []
-    assert_equal(result, @room1.playlist)
+    expected_result = []
+    assert_equal(expected_result, @room1.playlist)
+  end
+
+  def test_add_guest_to_room
+    @room1.add_guest_to_room(@guest1)
+    #We have called a new method .add_guest_to_room and we need to pass it an argument which is a 'real' guest object, in this case @guest1
+    actual_result = 1
+    expected_result = @room1.occupants.length()
+    assert_equal(actual_result, expected_result)
+  end
+
+  def test_remove_guest_from_room
+    @room1.add_guest_to_room(@guest1)
+    @room1.add_guest_to_room(@guest2)
+    @room1.add_guest_to_room(@guest3)
+    @room1.remove_first_guest_from_room()
+    #We don't need an argument for remove_guest here because we're not trying to remove by name, only kick out the first (or last) element in our array
+
+
+
   end
 
 end
